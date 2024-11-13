7627ec363264c5bb2021a1091dde934c1187c036
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-13 17:20:17 +0530

    Initial commit

---
+++ learn1.py
@@ -0,0 +1,16 @@
+# print('Welcome to Numpy Class')
+# import numpy as np
+# a = np.array([10,20,30,40,50])
+# print('Result A:',a)
+
+
+# import numpy as np
+# d = np.dtype([('name', 'S20'), ('perc', float)])
+# b = np.array([('apple', 3), ('cat', 1), ('bat', 5)], dtype=d)
+# print('Btype:', b)
+
+
+import numpy as np
+a = np.array([1,2,3,4,5])
+p = np.power(a,[2,3,3,3,4])
+print(p)
